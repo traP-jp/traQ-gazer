fd27a79860ac1f057079fe2756ce3c1a31796111
Luftalian <springtechtree@gmail.com>
2023-06-17 17:55:30 +0900

    merge

--- server/model/traq_message.go
+++ server/model/traq_message.go
@@ -3,9 +3,9 @@ package model
 import "h23s_15/api"
 
 func CheckMessageFromWords(message string) (api.UsersList, error) {
-	// words := WordsAllList{}
-	// if err := db.Select(&words, "SELECT * FROM words"); err != nil {
-	// 	return nil, err
-	// }
+	words := WordsAllList{}
+	if err := db.Select(&words, "SELECT * FROM words"); err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
