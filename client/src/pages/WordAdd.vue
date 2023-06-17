bdc315888cb25a5c1ff433b01d6cb4b25c4ac9bc
Pugma <maronmarondelicious@gmail.com>
2023-06-17 15:12:15 +0900

    npm run fmtを実行しました

--- client/src/pages/WordAdd.vue
+++ client/src/pages/WordAdd.vue
@@ -1,111 +1,110 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
-    import { Switch } from '@headlessui/vue'
-    import {
-        TransitionRoot,
-        TransitionChild,
-        Dialog,
-        DialogPanel,
-        DialogTitle,
-    } from '@headlessui/vue';
+import { ref } from 'vue'
+import { Switch } from '@headlessui/vue'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
 
-    const newBotNotify = ref(true)
-    const newSelfNotify = ref(false)
+const newBotNotify = ref(true)
+const newSelfNotify = ref(false)
 
-    const isFailedOpen = ref(false)
-    const isClearedOpen = ref(false)
+const isFailedOpen = ref(false)
+const isClearedOpen = ref(false)
 
-    function closeFailedDialog(){
-        isFailedOpen.value = false
-    }
-    function openFailedDialog(){
-        isFailedOpen.value = true
-    }
-    function closeClearedDialog(){
-        isClearedOpen.value = false
-    }
-    function openClearedDialog(){
-        isClearedOpen.value = true
-    }
+function closeFailedDialog() {
+  isFailedOpen.value = false
+}
+function openFailedDialog() {
+  isFailedOpen.value = true
+}
+function closeClearedDialog() {
+  isClearedOpen.value = false
+}
+function openClearedDialog() {
+  isClearedOpen.value = true
+}
 
-    interface Word {
-        word: string
-        botNotify: boolean
-        selfNotify: boolean
-    }
+interface Word {
+  word: string
+  botNotify: boolean
+  selfNotify: boolean
+}
 
-    const words = ref<Word[]>([
-    ])
-    const newWord = ref('')
+const words = ref<Word[]>([])
+const newWord = ref('')
 
-    const addWord = () => {
-      if(newWord.value.length>50){
-        openFailedDialog()
-      }
-      else if(newWord.value.length<=50 && newWord.value.length>0){
-        words.value.push({ word: newWord.value, botNotify: newBotNotify.value, selfNotify: newSelfNotify.value })
-        newWord.value = ""
-        openClearedDialog()
-      }
-    }
+const addWord = () => {
+  if (newWord.value.length > 50) {
+    openFailedDialog()
+  } else if (newWord.value.length <= 50 && newWord.value.length > 0) {
+    words.value.push({
+      word: newWord.value,
+      botNotify: newBotNotify.value,
+      selfNotify: newSelfNotify.value
+    })
+    newWord.value = ''
+    openClearedDialog()
+  }
+}
 </script>
 
 <template>
-    <title>新規単語追加ページ</title>
-    <header>
-        <p>traQエゴサ支援ツール</p>
-    </header>
-    <div class="expression">
-        <h1>新規単語の登録</h1>
-        <br>
-        <p>
-            以下のフォームで登録した単語がtraQ上に投稿された際、DMに通知を送信します。
-        </p>
-    </div>
-    <div>
-      <label>
-        <input v-model="newWord" type="text" placeholder="登録したい単語をここに入力(50文字以内)" class="inputForm"/>
-      </label>
-    </div>
-    <div class="bot">
-        <label>
-            <p>botの発言を通知する</p>
-            <Switch
-                v-model="newBotNotify"
-                :class="newBotNotify ? 'bg-teal-900' : 'bg-teal-700'"
-                class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-            >
-            <span class="sr-only">Use setting</span>
-            <span
-                aria-hidden="true"
-                :class="newBotNotify ? 'translate-x-9' : 'translate-x-0'"
-                class="pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
-            ></span>
-            </Switch>
-        </label>
-    </div>
-    <div class="myself">
-        <label>
-            <p>自分の発言を通知する</p>
-            <Switch
-                v-model="newSelfNotify"
-                :class="newSelfNotify ? 'bg-blue-600' : 'bg-gray-200'"
-                class="relative inline-flex h-6 w-11 items-center rounded-full"
-            >
-                <span class="sr-only">Enable notifications</span>
-                <span
-                    :class="newSelfNotify ? 'translate-x-6' : 'translate-x-1'"
-                    class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-                />
-            </Switch>
-        </label>
-    </div>
-    <div class="registerButton">
-        <button @click="addWord">登録</button>
-    </div>
+  <title>新規単語追加ページ</title>
+  <header>
+    <p>traQエゴサ支援ツール</p>
+  </header>
+  <div class="expression">
+    <h1>新規単語の登録</h1>
+    <br />
+    <p>以下のフォームで登録した単語がtraQ上に投稿された際、DMに通知を送信します。</p>
+  </div>
+  <div>
+    <label>
+      <input
+        v-model="newWord"
+        type="text"
+        placeholder="登録したい単語をここに入力(50文字以内)"
+        class="inputForm"
+      />
+    </label>
+  </div>
+  <div class="bot">
+    <label>
+      <p>botの発言を通知する</p>
+      <Switch
+        v-model="newBotNotify"
+        :class="newBotNotify ? 'bg-teal-900' : 'bg-teal-700'"
+        class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        <span class="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          :class="newBotNotify ? 'translate-x-9' : 'translate-x-0'"
+          class="pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+        ></span>
+      </Switch>
+    </label>
+  </div>
+  <div class="myself">
+    <label>
+      <p>自分の発言を通知する</p>
+      <Switch
+        v-model="newSelfNotify"
+        :class="newSelfNotify ? 'bg-blue-600' : 'bg-gray-200'"
+        class="relative inline-flex h-6 w-11 items-center rounded-full"
+      >
+        <span class="sr-only">Enable notifications</span>
+        <span
+          :class="newSelfNotify ? 'translate-x-6' : 'translate-x-1'"
+          class="inline-block h-4 w-4 transform rounded-full bg-white transition"
+        />
+      </Switch>
+    </label>
+  </div>
+  <div class="registerButton">
+    <button @click="addWord">登録</button>
+  </div>
 
-    <div>
-      <TransitionRoot appear :show="isFailedOpen" as="template">
+  <div>
+    <TransitionRoot appear :show="isFailedOpen" as="template">
       <Dialog as="div" @close="closeFailedDialog" class="relative z-10">
         <TransitionChild
           as="template"
@@ -120,9 +119,7 @@
         </TransitionChild>
 
         <div class="fixed inset-0 overflow-y-auto">
-          <div
-            class="flex min-h-full items-center justify-center p-4 text-center"
-          >
+          <div class="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as="template"
               enter="duration-300 ease-out"
@@ -135,16 +132,11 @@
               <DialogPanel
                 class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
               >
-                <DialogTitle
-                  as="h3"
-                  class="text-lg font-medium leading-6 text-gray-900"
-                >
+                <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
                   単語の登録に失敗しました
                 </DialogTitle>
                 <div class="mt-2">
-                  <p class="text-sm text-gray-500">
-                    登録する単語は50文字以内に収めてください。
-                  </p>
+                  <p class="text-sm text-gray-500">登録する単語は50文字以内に収めてください。</p>
                 </div>
 
                 <div class="mt-4">
@@ -179,9 +171,7 @@
         </TransitionChild>
 
         <div class="fixed inset-0 overflow-y-auto">
-          <div
-            class="flex min-h-full items-center justify-center p-4 text-center"
-          >
+          <div class="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as="template"
               enter="duration-300 ease-out"
@@ -194,10 +184,7 @@
               <DialogPanel
                 class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
               >
-                <DialogTitle
-                  as="h3"
-                  class="text-lg font-medium leading-6 text-gray-900"
-                >
+                <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
                   単語の登録に成功しました
                 </DialogTitle>
                 <div class="mt-2">
@@ -225,28 +212,28 @@
 </template>
 
 <style>
-.expression{
-    display: flex;
+.expression {
+  display: flex;
 }
-p{
-    font-size: 20px;
+p {
+  font-size: 20px;
 }
-.bot{
-    text-align: left;
-    display: flex;
+.bot {
+  text-align: left;
+  display: flex;
 }
-.myself{
-    text-align: left;
-    display: flex;
+.myself {
+  text-align: left;
+  display: flex;
 }
-.inputForm{
-    font-size: 20px;
-    width: 60%;
-    min-width: 10em;
-    max-width: 100%;
-    padding: 1.2em;
+.inputForm {
+  font-size: 20px;
+  width: 60%;
+  min-width: 10em;
+  max-width: 100%;
+  padding: 1.2em;
 }
-.regiserButton{
-    display: flex;
+.regiserButton {
+  display: flex;
 }
 </style>
--- client/src/pages/WordList.vue
+++ client/src/pages/WordList.vue
@@ -1,68 +1,66 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
+import { ref } from 'vue'
 
-    interface Word {
-        word: string
-        botNotify: boolean
-        selfNotify: boolean
-    }
-    const words = ref<Word[]>([
-        { word: 'ぷぐま', botNotify: true, selfNotify: true},
-        { word: 'C#', botNotify: true, selfNotify: true},
-        { word: 'C++', botNotify: false, selfNotify: true},
-        { word: 'うわぁ', botNotify: true, selfNotify: true},
-        { word: 'traP', botNotify: true, selfNotify: false},
-        { word: 'Twitter', botNotify: true, selfNotify: true},
-        { word: 'traQ', botNotify: false, selfNotify: false}
-    ])
+interface Word {
+  word: string
+  botNotify: boolean
+  selfNotify: boolean
+}
+const words = ref<Word[]>([
+  { word: 'ぷぐま', botNotify: true, selfNotify: true },
+  { word: 'C#', botNotify: true, selfNotify: true },
+  { word: 'C++', botNotify: false, selfNotify: true },
+  { word: 'うわぁ', botNotify: true, selfNotify: true },
+  { word: 'traP', botNotify: true, selfNotify: false },
+  { word: 'Twitter', botNotify: true, selfNotify: true },
+  { word: 'traQ', botNotify: false, selfNotify: false }
+])
 </script>
 
 <template>
-    <header>
-        traQエゴサ支援ツール
-    </header>
-    <div class="expression">
-        <h1>登録単語の閲覧ページ</h1>
-        <p>新規単語登録/登録単語の一覧/新規単語の検索/他の人が登録している単語</p>
-    </div>
-    <div class="table">
-        <table class="wordList">
-            <tr>
-                <th>単語</th>
-                <th>bot通知</th>
-                <th>自分の発言の通知</th>
-                <th>他の登録者</th>
-                <th></th>
-            </tr>
-            <tr v-for="item in words" :key="item.word">
-                <td>{{ item.word }}</td>
-                <td v-if="item.botNotify===true">ON</td>
-                <td v-if="item.botNotify===false">OFF</td>
-                <td v-if="item.selfNotify===true">ON</td>
-                <td v-if="item.selfNotify===false">OFF</td>
-            </tr>
-        </table>
-    </div>
+  <header>traQエゴサ支援ツール</header>
+  <div class="expression">
+    <h1>登録単語の閲覧ページ</h1>
+    <p>新規単語登録/登録単語の一覧/新規単語の検索/他の人が登録している単語</p>
+  </div>
+  <div class="table">
+    <table class="wordList">
+      <tr>
+        <th>単語</th>
+        <th>bot通知</th>
+        <th>自分の発言の通知</th>
+        <th>他の登録者</th>
+        <th></th>
+      </tr>
+      <tr v-for="item in words" :key="item.word">
+        <td>{{ item.word }}</td>
+        <td v-if="item.botNotify === true">ON</td>
+        <td v-if="item.botNotify === false">OFF</td>
+        <td v-if="item.selfNotify === true">ON</td>
+        <td v-if="item.selfNotify === false">OFF</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style>
-.expression{
-    text-align: left;
-    display: flex;
+.expression {
+  text-align: left;
+  display: flex;
 }
-.table{
-    overflow-x: scroll;
-    overflow-y: scroll;
+.table {
+  overflow-x: scroll;
+  overflow-y: scroll;
 }
-.wordList{
-    width: 1000px;
-    white-space: nowrap;
-    border-collapse: collapse;
+.wordList {
+  width: 1000px;
+  white-space: nowrap;
+  border-collapse: collapse;
 }
-th{
-    border-bottom: 1px solid white;
+th {
+  border-bottom: 1px solid white;
 }
-p{
-    font-size: 20px;
+p {
+  font-size: 20px;
 }
 </style>
