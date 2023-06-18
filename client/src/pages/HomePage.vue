f18a8a1532609b51b097cf9cf2ad3af90a486c2f
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-06-18 10:19:31 +0900

    テストとして書いていたものを削除

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import apiClient from '../apis'
-import { WordsAllList } from '../apis/generated'
 import PageLink from '../components/PageLink.vue'
-
-const list = ref<WordsAllList>()
-apiClient.words.getWords().then((value) => (list.value = value))
 </script>
 
 <template>
   <PageLink />
   <h1>Home</h1>
-  <p>{{ list }}</p>
 </template>
