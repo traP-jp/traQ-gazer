81b0fe45d262d55180737ddb391cf11b808ff254
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-06-18 13:47:45 +0900

    TitleLogo

--- client/src/components/TitleLogo.vue
+++ client/src/components/TitleLogo.vue
@@ -1,6 +1,8 @@
 <template>
   <router-link to="/">
-    <img src="../assets/traQgazer_logo.png" width="193" height="130" router-link to />
-    <p>traQエゴサ支援ツール</p>
+    <div class="flex items-center">
+      <img src="../assets/traQgazer_logo.png" class="w-2/5 m-8" />
+      <p class="text-xl">traQエゴサ支援ツール</p>
+    </div>
   </router-link>
 </template>
