8c2a304aaad345d5637b94247a972f398aa3f224
Pugma <maronmarondelicious@gmail.com>
2023-06-18 14:36:25 +0900

    これでGitのCheck通るかな…？

--- client/src/pages/WordList.vue
+++ client/src/pages/WordList.vue
@@ -52,7 +52,7 @@ const deleteWord = () => {
     <PageLink />
   </div>
   <div class="table">
-    <table class="wordList ">
+    <table class="wordList">
       <tr>
         <th class="">単語</th>
         <th class="">bot通知</th>
