0255e646dc0882b5a72b83246cd4907431a1754b
Pugma <maronmarondelicious@gmail.com>
2023-06-18 10:36:08 +0900

    単語の削除がAPIを通じてできるようになったと思います

--- client/src/pages/WordList.vue
+++ client/src/pages/WordList.vue
@@ -3,10 +3,54 @@ import { ref } from 'vue'
 import PageLink from '../components/PageLink.vue'
 import apiClient from '../apis'
 import { WordsAllList } from '../apis/generated'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { WordDelete } from '../apis/generated'
 
 const words = ref<WordsAllList>([])
 
+const isDeleteOpen = ref(false)
+
+const delWord = ref('')
+
 apiClient.words.getWords().then((res) => (words.value = res))
+
+const openEditDialog = () => {}
+
+const openDeleteDialog = () => {
+  isDeleteOpen.value = true
+}
+const closeDeleteDialog = () => {
+  isDeleteOpen.value = false
+}
+
+const deleteWord = () => {
+  const delBody: WordDelete = {
+    word: delWord.value
+  }
+
+  apiClient.words
+    .deleteWords(delBody)
+    .catch((v) => console.log(v))
+    .then(() => {
+      // 登録後のリストで更新
+      apiClient.words.getWords().then((res) => (words.value = res))
+    })
+
+  closeDeleteDialog()
+}
+
+const editMenu = [
+  {
+    name: '編集・設定',
+    onClick: openEditDialog
+  },
+  {
+    name: '削除',
+    onClick: openDeleteDialog
+  }
+]
 </script>
 
 <template>
@@ -28,8 +72,110 @@ apiClient.words.getWords().then((res) => (words.value = res))
         <td>{{ item.word }}</td>
         <td>{{ item.includeBot ? 'ON' : 'OFF' }}</td>
         <td>{{ item.includeMe ? 'ON' : 'OFF' }}</td>
+        <td></td>
+        <td>
+          <div class="editDialog">
+            <Popover v-slot="{ open }" class="relative">
+              <PopoverButton
+                :class="open ? '' : 'text-opacity-90'"
+                class="group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                <span>︙</span>
+                <ChevronDownIcon
+                  :class="open ? '' : 'text-opacity-70'"
+                  class="ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80"
+                  aria-hidden="true"
+                />
+              </PopoverButton>
+              <transition
+                enter-active-class="transition duration-200 ease-out"
+                enter-from-class="translate-y-1 opacity-0"
+                enter-to-class="translate-y-0 opacity-100"
+                leave-active-class="transition duration-150 ease-in"
+                leave-from-class="translate-y-0 opacity-100"
+                leave-to-class="translate-y-1 opacity-0"
+              >
+                <PopoverPanel
+                  class="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl"
+                >
+                  <div
+                    class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
+                  >
+                    <div class="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                      <button
+                        v-for="item in editMenu"
+                        :key="item.name"
+                        class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                        @click="item.onClick"
+                      >
+                        {{ item.name }}
+                      </button>
+                    </div>
+                  </div>
+                </PopoverPanel>
+              </transition>
+            </Popover>
+          </div>
+        </td>
       </tr>
     </table>
+  </div>
+
+  <div>
+    <TransitionRoot appear :show="isDeleteOpen" as="template">
+      <Dialog as="div" @close="closeDeleteDialog" class="relative z-10">
+        <TransitionChild
+          as="template"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-black bg-opacity-25" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 overflow-y-auto">
+          <div class="flex min-h-full items-center justify-center p-4 text-center">
+            <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0 scale-95"
+              enter-to="opacity-100 scale-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100 scale-100"
+              leave-to="opacity-0 scale-95"
+            >
+              <DialogPanel
+                class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+              >
+                <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
+                  この単語を削除しますか？
+                </DialogTitle>
+
+                <div class="mt-4">
+                  <button
+                    type="button"
+                    class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    @click="deleteWord"
+                  >
+                    削除する
+                  </button>
+                  <button
+                    type="button"
+                    class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    @click="closeDeleteDialog"
+                  >
+                    キャンセル
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
   </div>
 </template>
 
