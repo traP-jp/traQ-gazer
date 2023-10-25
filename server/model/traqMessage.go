01d2978c4d3e916a0e39b1bd3f40469b32fb011d
oribe <oribe.oribe1115@gmail.com>
2023-10-25 12:18:00 +0900

    検索をスマートにまとめようとしたが、これでは単語ごとに通知設定が違うことが考慮できてない

--- server/model/traqMessage.go
+++ server/model/traqMessage.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"strings"
 
 	"golang.org/x/exp/slog"
@@ -80,6 +81,67 @@ func TraqMessageProcessor(messageList MessageList) (SendList, error) {
 	return sendList, nil
 }
 
+func FindMatchingWords(messageList MessageList) ([]*NotifyInfo, error) {
+	notifyInfoList := make([]*NotifyInfo, 0)
+
+	// メッセージごとに通知対象を検索する
+	for _, messageItem := range messageList {
+		// メッセージの送信者の情報を取得
+		var sender User
+		err := db.Get(&sender,
+			`SELECT * FROM users WHERE traq_uuid = ?`,
+			messageItem.TraqUuid)
+		if err != nil {
+			return nil, err
+		}
+
+		// メッセージに含まれている登録単語全てを取得し、登録者別にまとめる
+		matchedWordsList := make([]*MatchedWords, 0)
+		err = db.Select(&matchedWordsList, `
+			SELECT
+				group_concat(words.word SEPARATOR ':::') AS contacted_words,
+				words.trap_id,
+				users.traq_uuid,
+				bot_notification,
+				me_notification
+			FROM words
+			JOIN users ON words.trap_id = users.trap_id
+				WHERE ? LIKE concat('%', word, '%')
+			GROUP BY words.trap_id`,
+			messageItem.Content)
+		if err != nil {
+			slog.Error(fmt.Sprintf("failed to search words with message: `%s`", messageItem.Id))
+			return nil, err
+		}
+
+		for _, matchedWords := range matchedWordsList {
+			// 登録設定に応じて、通知しないものを除外
+			if !matchedWords.MeNotification {
+				if matchedWords.TraqUUID == sender.Traq_uuid {
+					continue
+				}
+			}
+			if !matchedWords.BotNotification {
+				if sender.Is_bot {
+					continue
+				}
+			}
+
+			notifyInfo := &NotifyInfo{
+				Words:                strings.Split(matchedWords.ContactedWords, ":::"),
+				NotifyTargetTrapId:   matchedWords.TrapID,
+				NotifyTargetTraqUuid: matchedWords.TraqUUID,
+				MessageId:            messageItem.Id,
+			}
+
+			notifyInfoList = append(notifyInfoList, notifyInfo)
+		}
+
+		return notifyInfoList, nil
+	}
+
+}
+
 type MessageItem struct {
 	// メッセージUUID
 	Id string `json:"id"`
@@ -118,3 +180,21 @@ type Send struct {
 }
 
 type SendList []*Send
+
+type NotifyInfo struct {
+	Words []string
+	// 送信先のuser
+	NotifyTargetTrapId string
+	// 送信先のuserUUID
+	NotifyTargetTraqUuid string
+	// 送信するメッセージのID
+	MessageId string
+}
+
+type MatchedWords struct {
+	ContactedWords  string `db:"contacted_words"`
+	TrapID          string `db:"trap_id"`
+	TraqUUID        string `db:"traq_uuid"`
+	BotNotification bool   `db:"bot_notification"`
+	MeNotification  bool   `db:"me_notification"`
+}
