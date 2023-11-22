aee7311c41a86333ef4c610821cc9878e154a3e6
Pugma <maronmarondelicious@gmail.com>
2023-11-22 23:17:16 +0900

    これで自分の通知の変更を反映させるバックエンド部分できたかと思います

---
+++ server/model/me.go
@@ -0,0 +1,11 @@
+package model
+
+func ChengeMeNotification(word string, includeMe bool, userId string) error {
+	_, err := db.Exec("UPDATE words SET me_notification = ? WHERE word = ? AND trap_id = ?", includeMe, word, userId)
+	return err
+}
+
+func ChangeAllMeNotification(includeMe bool, userId string) error {
+	_, err := db.Exec("UPDATE words SET me_notification = ? WHERE trap_id = ?", includeMe, userId)
+	return err
+}
