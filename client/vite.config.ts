c6faddc1edeb87d70d7ae207f7bf42202af58003
Pugma <maronmarondelicious@gmail.com>
2024-06-30 16:55:18 +0900

    tailwindの削除 / scssへの変更 / prismを手元でも使えるように

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+
+const srcPath = path.resolve(import.meta.dirname, 'src').replace(/\\/g, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4010',
+        rewrite: path =>
+          path.startsWith('/api') ? path.slice('/api'.length) : path,
+        changeOrigin: true
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "${srcPath}/styles/color.scss";
+        @import "${srcPath}/styles/mixin.scss";
+        `
+      }
+    }
+  },
   plugins: [vue()]
 })
