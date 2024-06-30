0bf2ee4e9f44152cc16f837f6e4e1ca2b22b1f3b
Pugma <maronmarondelicious@gmail.com>
2024-07-01 01:17:31 +0900

    通知設定のスタイルを修正

--- client/src/components/NotifySwitch.vue
+++ client/src/components/NotifySwitch.vue
@@ -1,35 +1,37 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
 import { Icon } from '@iconify/vue'
 
-const notify = ref<string>('true')
 defineProps<{ title: string }>()
 
 const model = defineModel<boolean>({ required: true })
-
-watch(notify, () => {
-  model.value = notify.value === 'true' ? true : false
-})
 </script>
 
 <template>
   <div class="switch">
     <div>{{ title }}</div>
-    <Icon
-      :icon="model ? 'mdi:notifications-active' : 'mdi:notifications-off'"
-      width="40"
-      height="40"
-      class="notify"
-      @click="model = !model"
-    />
+    <div class="icon">
+      <Icon
+        :icon="model ? 'mdi:notifications-active' : 'mdi:notifications-off'"
+        width="40"
+        height="40"
+        class="notify"
+        @click="model = !model"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .switch {
-  display: block;
   padding: 4px;
+  justify-content: center;
 }
+
+.icon {
+  display: flex;
+  margin: auto;
+}
+
 .notify {
   cursor: pointer;
 }
