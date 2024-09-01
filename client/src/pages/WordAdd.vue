349b1be7d4cb9e53733d468ec3ae09612712ef75
Pugma <maronmarondelicious@gmail.com>
2024-09-01 15:45:57 +0900

    fix layout of tables and section containers

--- client/src/pages/WordAdd.vue
+++ client/src/pages/WordAdd.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import NotifySwitch from '../components/NotifySwitch.vue'
 import WordList from '../components/WordList.vue'
-import SectionContainer from '../components/ArticleContainer.vue'
+import ArticleContainer from '../components/ArticleContainer.vue'
 
 import apiClient from '../apis'
 import { WordRequest, WordsList } from '../apis/generated'
@@ -43,38 +43,55 @@ const update = () => {
 
 <template>
   <PageContainer>
-    <section-container
+    <article-container
       title="新規単語の登録"
-      description="以下のフォームで登録した単語がtraQ上に投稿された際、DMに通知を送信します。"
+      description="以下のフォームで登録した単語がtraQ上に投稿された際、DM で通知を送信します"
     >
-      <section class="form">
+      <section :class="$style.form">
         <input
+          size="1"
           v-model="newWord"
           type="text"
           placeholder="登録したい単語をここに入力(50文字以内)"
-          class="inputForm"
+          :class="$style.inputForm"
           @keypress.enter="registerNewWord"
         />
-        <section>
-          <h3>通知設定</h3>
-          <div class="settings">
-            <NotifySwitch v-model:notify="newBotNotify" title="Botの投稿" />
-            <NotifySwitch v-model:notify="newSelfNotify" title="自分の投稿" />
+        <section
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            flex-shrink: 0;
+            gap: 16px;
+          "
+        >
+          <section>
+            <h2>通知設定</h2>
+            <div :class="$style.settings">
+              <NotifySwitch v-model:notify="newBotNotify" title="Botの投稿" />
+              <NotifySwitch v-model:notify="newSelfNotify" title="自分の投稿" />
+            </div>
+          </section>
+          <div :class="$style.registerButton">
+            <primary-button text="登録" :disabled="newWord" @click="registerNewWord" />
           </div>
         </section>
-        <div class="registerButton">
-          <primary-button text="登録" :disabled="newWord === ''" @click="registerNewWord" />
-        </div>
       </section>
-    </section-container>
+    </article-container>
 
-    <section-container title="登録単語の閲覧" class="wordList">
-      <word-list class="wordList" :words="words" @update="update()" />
-    </section-container>
+    <article-container
+      title="登録単語の閲覧"
+      description="登録した単語の通知設定を変更したり、単語の登録を解除したりできます"
+      :class="$style.wordList"
+    >
+      <div :class="$style.wordList">
+        <word-list :class="$style.wordList" :words="words" @update="update()" />
+      </div>
+    </article-container>
   </PageContainer>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" module>
 .form {
   display: flex;
   flex-wrap: wrap;
@@ -82,16 +99,13 @@ const update = () => {
   align-items: center;
   padding: 8px;
   gap: 16px;
-
-  @include sp {
-    display: block;
-  }
 }
 
 .inputForm {
-  width: 90vw;
-  min-width: 300px;
-  max-width: 500px;
+  width: 55%;
+  flex-grow: 2;
+  min-width: 350px;
+  max-width: 80%;
   padding: 1.25rem;
   border-radius: 8px;
   margin: 0px 4px;
@@ -100,12 +114,13 @@ const update = () => {
 
 .settings {
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   padding: 8px;
   gap: 8px;
 }
 
 .wordList {
   margin: auto;
+  overflow-x: scroll;
 }
 </style>
